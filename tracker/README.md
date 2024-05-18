794401119e4e3a3caced6b9ac51e08effefc4498
AnastasiiaZhukova <116913734+AnastasiiaZhukova@users.noreply.github.com>
2024-05-18 19:34:41 +0100

    Delete tracker directory

--- tracker/README.md
+++
@@ -1,8 +0,0 @@
-```properties
-spring_profiles_active=prod
-PROD_DB_HOST=postgres.railway.internal
-PROD_DB_PORT=53454
-PROD_DB_NAME=railway
-PROD_DB_PASSWORD=xXjkdpidvpaFbEXHQKeRMzblJvRBGkiU
-PROD_DB_USERNAME=postgres
-```
--- tracker/src/main/resources/static/js/checkIfPasswordsMatch.js
+++
@@ -1,9 +0,0 @@
-function checkIfPasswordsMatch() {
-    var password = document.getElementById('InputPassword').value;
-    var confirmPassword = document.getElementById('InputConfirmPassword').value;
-    if (password !== confirmPassword) {
-        alert('Passwords do not match!');
-        return false;
-    }
-    return true;
-}
